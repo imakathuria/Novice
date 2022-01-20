1ef5803660a9867fb27a4c972562185ec6b5f3ee
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-21 01:59:58 +0530

    Time: 2 ms (87.23%), Space: 9.7 MB (72.80%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int index=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        return index;
-        // int low=0,high=nums.size()-1;
-        // while(low<high){
-        //     int mid=low+(high-low)/2;
-        //     if(nums[mid]==target){
-        //         return mid;
-        //     }
-        //     if(nums[mid]<target){
-        //         low=mid+1;
-        //     }else if(nums[mid]>target){
-        //         high=mid-1;
-        //     }
-        // }
-        // if(nums[high]<target){
-        //     return high+1;
-        // }
-        // else if(nums[high]>target){
-        //     return high;
-        // }
-        // return high;
+        // int index=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        // return index;
+        if(target < nums[0]) return 0;
+        int low=0,high=nums.size()-1;
+        if(target > nums[nums.size() - 1]) return high+1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<target){
+                low=mid+1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        if(nums[high]<target){
+            return high+1;
+        }
+        else if(nums[high]>target){
+            return high;
+        }
+        return high;
     }
 };
