598601a52272bfd814199999c7075723976d49fc
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-21 02:07:49 +0530

    Time: 107 ms (42.04%), Space: 55.3 MB (8.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& arr) {
-        unordered_map<int,int> m;
-        for(int i=0;i<arr.size();i++){
-            m[arr[i]]++;
-            if(m[arr[i]]>1)return true;
-        }
-        return false;
+        unordered_set<int> s(arr.begin(),arr.end());
+        return !(s.size()==arr.size());
+        // unordered_map<int,int> m;
+        // for(int i=0;i<arr.size();i++){
+        //     m[arr[i]]++;
+        //     if(m[arr[i]]>1)return true;
+        // }
+        // return false;
     }
 };
