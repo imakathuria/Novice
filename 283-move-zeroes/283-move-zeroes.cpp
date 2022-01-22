c9aac224d5d4f1226a8417130a6b4f85fa0bab6e
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-22 14:59:29 +0530

    Time: 28 ms (50.87%), Space: 19.3 MB (13.24%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int zerosCount=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]==0)zerosCount++;
-        // }
-        // int k=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]!=0){
-        //         nums[k++]=nums[i];
-        //     }
-        // }
-        // while(k<nums.size()){
-        //     nums[k++]=0;
-        // }
-        // return;
         int j=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
