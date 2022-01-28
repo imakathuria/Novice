ed648850923aea2d42d50762b1c96770cbb40b9e
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-28 23:53:31 +0530

    Time: 39 ms (28.35%), Space: 17.9 MB (97.10%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,38 +1,19 @@
-#include<bits/stdc++.h>
-#include <string>
-#include<sstream>
-using namespace std;
 class Solution {
 public:
-    bool isvalid(char c,int row,int col,vector<vector<char>>board){
-        for(int i=0;i<9;i++){
-            if(board[row][i]==c && i!=col){
-                return false;
-            }
-            if(board[i][col]==c && i!=row){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+ i %3]==c && (3*(row/3)+i/3) !=row && (3*(col/3)+ i %3)!=col ){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    bool solve(vector<vector<char>>&board){
+    bool isValidSudoku(vector<vector<char>>& a) {
+        int row[9][9]={0},col[9][9]={0},box[9][9]={0};
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                    if(!isvalid(board[i][j],i,j,board)){
-                       return false;
-                    }
-                   
+                if(a[i][j] != '.'){
+                    int t=a[i][j]-'0'-1;
+                    int k=(i/3)*3+(j)/3;    // for calculating the box number
+                    if(row[t][j] || col[i][t] || box[k][t])
+                        return 0;
+                    else
+                        row[t][j]=col[i][t]=box[k][t]=1;
                 }
             }
         }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        return solve(board);
+        return 1;
     }
 };
