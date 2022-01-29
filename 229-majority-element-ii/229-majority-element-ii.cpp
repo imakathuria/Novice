5fcfbc843a05f0bdab17abdb08c069331f39f8e9
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-29 13:01:51 +0530

    Time: 10 ms (83.72%), Space: 15.7 MB (96.02%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -4,17 +4,59 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> ump;
+        // Boyer Moore Voting method
+        int minct = nums.size()/3;
+        int c1,c2,m1=INT_MIN,m2=INT_MIN;
+        c1=c2=0;
+        
         for(auto i : nums){
-            ump[i]++;
-        }
-        int minct=(nums.size())/3;
-        vector<int> ans;
-        for(auto it=ump.begin();it!=ump.end();it++){
-            if(it->second > minct){
-                ans.pb(it->first);
+            if(i==m1){
+                c1++;
+            }
+            else if(i==m2){
+                c2++;
+            }
+            else if(c1==0){
+                m1 = i;
+                c1=1;
+            }else if(c2==0){
+                m2 = i;
+                c2=1;
+            }else{
+                c1--;
+                c2--;
             }
         }
-        return ans;
+        // cout<<m1<<m2<<endl;
+        int cm1,cm2;
+        cm1=cm2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==m1){
+                cm1++;
+            }else if(nums[i]==m2){
+                cm2++;
+            }
+        }
+        vector<int> v;
+        if(cm1>minct){
+            v.pb(m1);
+        }
+        if(cm2>minct){
+            v.pb(m2);
+        }
+        return v;
+        
+        // unordered_map<int,int> ump;
+        // for(auto i : nums){
+        //     ump[i]++;
+        // }
+        // int minct=(nums.size())/3;
+        // vector<int> ans;
+        // for(auto it=ump.begin();it!=ump.end();it++){
+        //     if(it->second > minct){
+        //         ans.pb(it->first);
+        //     }
+        // }
+        // return ans;
     }
 };
