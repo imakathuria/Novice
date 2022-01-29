ea927364e6e98aae4289304da17897e1dca09bad
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-01-29 14:18:39 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -17,7 +17,19 @@ public:
 
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return up(m,n,0,0,dp);
+        // vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        // return up(m,n,0,0,dp);
+        // 10C3 --> (8*9*10/1*2*3)
+        // Question can be solved with the combination of left and right paths
+        // because we know possible Bottom and right paths
+        int RightPaths = n-1;
+        int BottomPaths = m-1;
+        int TotalPaths = m+n-2;
+        // So possible combinations could be (TotalPaths C BottomPaths) or (TotalPaths C RightPaths) 
+        double UniquePaths = 1;
+        for(int i=1;i<=RightPaths;i++){
+            UniquePaths = UniquePaths * (TotalPaths - RightPaths + i)/i;
+        }
+        return (int)UniquePaths;
     }
 };
