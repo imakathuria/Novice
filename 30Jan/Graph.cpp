227aab0c7507039c36c5247ff38d859adb7ed94a
imakathuria <imakathuria@gmail.com>
2022-01-30 18:01:10 +0530

    BFS

--- 30Jan/Graph.cpp
+++ 30Jan/Graph.cpp
@@ -1,44 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 // #include <ext/pb_ds/assoc_container.hpp>
 // using namespace __gnu_pbds;
 using namespace std;
 
-#define ff              first
-#define ss              second
-#define int             long long
-#define pb              push_back
-#define mp              make_pair
-#define pii             pair<int,int>
-#define vi              vector<int>
-#define mii             map<int,int>
-#define pqb             priority_queue<int>
-#define pqs             priority_queue<int,vi,greater<int> >
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-#define mod             1000000007
-#define inf             1e18
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define mk(arr,n,type)  type *arr=new type[n];
-#define w(x)            int x; cin>>x; while(x--)
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define ff first
+#define ss second
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 // typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 void c_p_c()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 }
-int32_t main()
+
+vector<int> Bfs_Of_Graph(vector<int> adj[],int n){
+    vector<bool> visited(n+1,0);
+    vector<int> bfs;
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            queue<int> q;
+            q.push(i);
+            visited[i]=1;
+            while(!q.empty()){
+                int front = q.front();
+                q.pop();
+                bfs.pb(front);
+                for(auto it : adj[front]){
+                    if(!visited[it]){
+                        q.push(it);
+                        visited[it]=1;
+                    }
+                }
+            }
+            bfs.pb(0);
+        }
+    }
+    return bfs;
+
+
+}
+void graph()
 {
-	// c_p_c();
-    int vertices,edges;
+    int vertices, edges;
     // One based indexing graph
     // Made using matrix
-    cin>>vertices>>edges;
+    cin >> vertices >> edges;
     /*
     int adj[vertices+1][vertices+1];
     for(int i=0;i<edges;i++){
@@ -49,12 +80,28 @@ int32_t main()
     }
     */
     // space complexity using matrix is O(n*n)
-    vector<int> adj[vertices+1];
-    for(int i=1;i<=edges;i++){
-        int u,v;
-        cin>>u>>v;
+    vector<int> adj[vertices + 1];
+    for (int i = 1; i <= edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
         adj[u].pb(v);
         adj[v].pb(u);
     }
-	return 0;
+    vector<int> bfs = Bfs_Of_Graph(adj,vertices);
+    for(auto i:bfs){
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
+int32_t main()
+{
+    // c_p_c();
+    graph();
+    return 0;
+}
+/*
+TC
+5 4 1 2 3 4 4 5 5 3
+5 4 1 4 2 5 3 5 2 3
+*/
