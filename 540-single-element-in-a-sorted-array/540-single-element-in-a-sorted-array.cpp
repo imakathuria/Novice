acc393b20c16a053feba2baa1b9e87ace1e4db12
Akhil Kathuria <63406522+imakathuria@users.noreply.github.com>
2022-02-01 13:28:51 +0530

    Time: 63 ms (5.33%), Space: 22.3 MB (91.15%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> ump;
-        for(int i=0;i<nums.size();i++){
-            ump[nums[i]]++;
-        }
-        for(auto it:ump){
-            if(it.second==1){
-                return it.first;
+        // unordered_map<int,int> ump;
+        // for(int i=0;i<nums.size();i++){
+        //     ump[nums[i]]++;
+        // }
+        // for(auto it:ump){
+        //     if(it.second==1){
+        //         return it.first;
+        //     }
+        // }
+        // return 0;
+        int low=0,high=nums.size()-2;
+        while(low<=high){
+            int mid=(low+high)>>1;
+            if(nums[mid]==nums[mid^1]){
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        return 0;
+        return nums[low];
     }
 };
